71a9b32ce63cddc7f52f69fede872015aa54970d
Stefan Dzisiewski-Smith <stefan.dzisiewski.smith@gmail.com>
2013-10-03 23:36:01 +0100

    Moving towards a struct / object combo to ease pulling setup data out into a separate file

--- mpr121.h
+++ mpr121.h
@@ -1,6 +1,14 @@
 #ifndef MPR121_H
 #define MPR121_H
 
+// idea behind this is to create a settings structure that we can use to store all the
+// setup variables for a particular setup - comes pre-instantiated with defaults and
+// can be easily tweaked - we pass by reference (as a pointer) to save RAM
+struct MPR121_settings
+{
+	
+};
+
 class MPR121_t
 {
 	private:
@@ -9,13 +17,14 @@ class MPR121_t
 		unsigned char releaseThr[13];
 	public:
 		MPR121_t();
-		void begin(unsigned char address);	// I think you only need to be able to set the address
-									 		// at the very beginning
+		void begin(unsigned char address);	// I think you only need to be able to set the
+											// address at the very beginning
 		void begin(); 
 		void run();
 		void stop();
 		void reset();
 		void enableTouchElectrodes();
+		void applySettings(MPR121_settings *settings);
 		
 		void setTouchThreshold(unsigned char val);
 		void setTouchThreshold(unsigned char val, unsigned char electrode);
